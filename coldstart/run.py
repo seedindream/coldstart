54065091b880a9917bcf156ccfd9c16c65c5f067
seedindream <li.7533@buckeyemail.osu.edu>
2017-11-29 11:45:00 -0500

    five

--- coldstart/run.py
+++ coldstart/run.py
@@ -9,7 +9,7 @@ cmdCommand = 'powershell D:\\home\\site\\wwwroot\\coldstart\\record.exe' #specif
 process = subprocess.Popen(cmdCommand.split(), shell=True,stdout=subprocess.PIPE)
 output5, error = process.communicate()
 
-postreqdata = json.loads(open(os.environ['req']).read())
+# postreqdata = json.loads(open(os.environ['req']).read())
 response = open(os.environ['res'], 'w')
 response.write("response time "+str(tm_st)+"@\n"+output5)
 response.close()
