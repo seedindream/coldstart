36d3f9b1ecc630e17602b40e3cc24eddbed52bf1
seedindream <li.7533@buckeyemail.osu.edu>
2017-11-29 11:29:29 -0500

    four

--- coldstart/run.py
+++ coldstart/run.py
@@ -11,5 +11,5 @@ output5, error = process.communicate()
 
 postreqdata = json.loads(open(os.environ['req']).read())
 response = open(os.environ['res'], 'w')
-response.write("response time "+str(tm_st)+"@"+output5)
+response.write("response time "+str(tm_st)+"@\n"+output5)
 response.close()
