3986c9a5d968dc73d71dd7409c1873de16e74de4
seedindream <li.7533@buckeyemail.osu.edu>
2017-12-11 00:17:10 -0500

    4 times

--- coldstart/run.py
+++ coldstart/run.py
@@ -8,8 +8,9 @@ tm_st = time.time()
 cmdCommand = 'powershell D:\\home\\site\\wwwroot\\coldstart\\record.exe' #specify your cmd command
 process = subprocess.Popen(cmdCommand.split(), shell=True,stdout=subprocess.PIPE)
 output5, error = process.communicate()
+tm_finish = time.time()
 
 # postreqdata = json.loads(open(os.environ['req']).read())
 response = open(os.environ['res'], 'w')
-response.write("response time "+str(tm_st)+"@\n"+output5)
+response.write("response time "+str(tm_st)+"finish "+str(tm_finish)+"@\n"+output5)
 response.close()
